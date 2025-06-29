9c7337881a1578f289b35ece396545d8b3799ef7
pixel-prady <pradhumanchaudhary580a@gmail.com>
2025-06-29 18:13:29 +0530

    util files added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/connection.js";
-
+import {app} from "./app.js"
+ 
 dotenv.config({
-    path:"./.env"
-})
+    path: "./.env",
+});
 
 console.log(process.env.MONGODB_URI);
 
 connectDB()
+    .then(() => {
+        const server = app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running at ${process.env.PORT}`);
+        });
+        server.on("error", (error) => {
+            console.log("SERVER ERROR : ", error);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGODB CONNECTION FAILED : ", err);
+    });
