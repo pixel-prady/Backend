4e838fb98b8af6ee0d5dd6034debebdf2eade7f7
pixel-prady <pradhumanchaudhary580a@gmail.com>
2025-06-30 06:45:20 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -12,8 +12,14 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb" }));
-app.unsubscribe(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration: 
+app.use("/api/v1/users",userRouter)
 
 export { app };
